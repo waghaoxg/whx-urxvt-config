a36d5779be2b525986699f5bb1066e3dd5486d19
wanghaoxing <wanghaoxing1@xiaomi.com>
2022-05-21 10:29:52 +0800

    1.change bg color; 2.add install script

---
+++ install_ext.sh
@@ -0,0 +1,16 @@
+EXT_DIR=~/.urxvt/ext
+BUNDLE_DIR=~/.urxvt/bundle
+if [ -d "$EXT_DIR" ];
+then
+    echo "$DIR directory exists. Doing nothing"
+else
+    mkdir $EXT_DIR
+    mkdir $BUNDLE_DIR
+    cd $BUNDLE_DIR
+    #install autocomplete
+    git clone https://github.com/vifon/autocomplete-ALL-the-things.git
+    cp autocomplete-ALL-the-things/autocomplete-ALL-the-things $EXT_DIR
+    #install keyboard
+    git clone https://github.com/muennich/urxvt-perls.git 
+    cp urxvt-perls/keyboard-select $EXT_DIR
+fi
